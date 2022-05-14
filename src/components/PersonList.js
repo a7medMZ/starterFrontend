1834d4d2c1e0ca3f3c96fd1c4afc8ac70e4d44fc
Ahmed <ahmed.mzoughi@infor.com>
2022-05-14 10:41:11 +0100

    adding alb dns

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
+    axios.get(`http://initi-loadb-9ou3765pa8ig-966396263.eu-central-1.elb.amazonaws.com:8080/welcome`)
       .then(res => {
         const persons = res.data;
         this.setState({ persons });
